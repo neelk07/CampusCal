7ef89894b250a46db5263494f143b760259b95bb
Neel Kothari <neel@Neels-MacBook-Air.local>
2013-11-02 17:18:00 -0500

    graph search works

--- events/views.py
+++ events/views.py
@@ -15,6 +15,8 @@ from django_facebook.connect import CONNECT_ACTIONS, connect_user
 from django_facebook.decorators import facebook_required_lazy
 from django_facebook.utils import next_redirect, get_registration_backend, \
     to_bool, error_next_redirect, get_instance_for
+from open_facebook import exceptions as open_facebook_exceptions
+from open_facebook.utils import send_warning
 ITEMS_PER_PAGE = 10
 
 
@@ -22,11 +24,20 @@ ITEMS_PER_PAGE = 10
 def landing_page(request):
     context = RequestContext(request)
     return render_to_response('landing.html', context)
-    
+
 
 @facebook_required_lazy
-def recent_events_page(request):
+def recent_events_page(request,graph):
 	#recent_events = Event.objects.order_by('-created')[:10]
+
+	me = graph.get('me')
+	friends = graph.get('me/friends')
+	feed = graph.get('me/feed')
+	name = me['languages']
+
+	for n in name:
+		print n['name']
+
 	recent_events = Event.objects.raw('SELECT "events_event"."id", "events_event"."created", "events_event"."title", "events_event"."description", "events_event"."location", "events_event"."host", "events_event"."date", "events_event"."male", "events_event"."female", "events_event"."facebook_link" FROM "events_event" ORDER BY "events_event"."created" DESC LIMIT 10')
 	variables = RequestContext(request,
 		{
@@ -36,18 +47,16 @@ def recent_events_page(request):
 
 
 
-def _event_save(request, form):
-            
-			#retreive host 
-			
+def _event_save(request, form, graph):
 
             # Create or get event.
             event, created = Event.objects.get_or_create(
-              title = form.cleaned_data['title'],
-              description = form.cleaned_data['description'],
-              location = form.cleaned_data['location'],
-              time = form.cleaned_data['time']
+              	title = form.cleaned_data['title'],
+              	description = form.cleaned_data['description'],
+              	location = form.cleaned_data['location'],
+              	time = form.cleaned_data['time']
             )
+
             # Update event title
           
             # Update event description.
